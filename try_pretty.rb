e5a8522021bdd1b27ef16df853443665f64c3ada
Barbara Slawinska <slawinska.barbara@gmail.com>
2013-08-21 10:08:53 +0200

    Corrected version

--- try_pretty.rb
+++ try_pretty.rb
@@ -7,25 +7,31 @@
 			elem.max_by {|el| el.length}
 		end
 		m = new_array.max_by {|elem| elem.length}
-
-
-		longest_sub_array = array.max_by {|elem| elem.length}
-		w = longest_sub_array.to_s.length - 2
+		z = array[0].size
+		w = m.size * z + z + 2
+		
 
 
 		string = ""
 		array.each do |sub_array|
-			string << " " + "-" * w
+			string << " "
+			string << ("+" + "-" * m.size + "-") * z + "+"
 			string << "\n"
 			sub_array.each do |elem|
 				string << " |" + elem.ljust(m.size) 
 			end
-			string << "|"
+			string << " |"
 			string << "\n"
 			
 		end
-		string << " " + "-" * w
+		string << " "
+		string << ("+" + "-" * m.size + "-") * z + "+"
 		string << "\n"
 		
 	end
+
 	print pretty_table(array)
+	#array = [['Number', 'Name', 'ID'], [1, 'Foo', 'Zoo'], [2, 'Bar', 'Yoo'], [3, 'Baz', 'Bahhhhhhhhhhhhh']]
+	#print pretty_table(array)
+	#array = [['Number', 'Name', 'ID', 'Urg'], [1, 'Foo', 'Zoo', 'Gooo'], [2, 'Bar', 'Yoo', 7], [1, 3, 'Baz', 'Bahhhhhhhhhhhhh']]
+	#print pretty_table(array)
